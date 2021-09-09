e1358fbe473f00e1af09ec75327b563095c1a578
Sam Kumar <samkumar@berkeley.edu>
2021-09-09 15:42:38 -0600

    Update tape scripts for latest experiments

--- deploy_oblivious_on_cloudlab.sh
+++ deploy_oblivious_on_cloudlab.sh
@@ -134,6 +134,7 @@ then
         echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
         echo "aslr : $(cat /proc/sys/kernel/randomize_va_space)"
         echo "transparent huge pages: $(cat /sys/kernel/mm/transparent_hugepage/enabled)"
+	echo 0 | sudo tee /proc/sys/kernel/numa_balancing
 
         sudo ifconfig $IFACE 10.0.0.1 netmask 255.0.0.0 up
         pushd /mydata/oblivious/syncswap/drivers
