cb00b4ade7eb063b4f73d2ad426aef3bc617a679
Sam Kumar <samkumar@berkeley.edu>
2021-09-15 15:13:52 -0700

    Add script for Sep. 12 tracing experiments

--- sep_12_tracing_experiments.sh
+++ sep_12_tracing_experiments.sh
@@ -9,34 +9,36 @@ then
 	ln -s /mydata/traces /data/traces
 fi
 
-#sudo rm -rf /data/traces/*
-#yes | sudo ./benchmark.sh python 107903 taskset -c 0 ~/miniconda3/bin/python python/mmult.py 4 4096 mat
+export US_SIZES="1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536"
+export RATIOS="30"
 
 #sudo rm -rf /data/traces/*
-#yes | sudo ./benchmark.sh mmult_eigen 101394 taskset -c 0 ./cpp/mmult_eigen 4 4096 mat
-sudo US_SIZES="8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536" RATIOS="30" ./trace_benchmark.sh mmult_eigen 101394 taskset -c 0 ./cpp/mmult_eigen 4 4096 mat
+sudo ./trace_benchmark.sh python 107903 taskset -c 0 ~/miniconda3/bin/python python/mmult.py 4 4096 mat
+
+#sudo rm -rf /data/traces/*
+sudo ./trace_benchmark.sh mmult_eigen 101394 taskset -c 0 ./cpp/mmult_eigen 4 4096 mat
 
 #sudo rm -rf /data/traces/*
 #mkdir /data/traces/mmult_eigen_par/
-#yes | sudo GOMP_CPU_AFFINITY="0-1" OMP_SCHEDULE=static OMP_NUM_THREADS=2 ./benchmark.sh mmult_eigen_par 101677 taskset -c 0,1 ./cpp/mmult_eigen_par 43 4096 mat
-#sudo mv experiment_results/mmult_eigen_par experiment_results/mmult_eigen_par_2
+sudo GOMP_CPU_AFFINITY="0-1" OMP_SCHEDULE=static OMP_NUM_THREADS=2 ./trace_benchmark.sh mmult_eigen_par 101677 taskset -c 0,1 ./cpp/mmult_eigen_par 43 4096 mat
+sudo mv tracing_results/mmult_eigen_par tracing_results/mmult_eigen_par_2
 
 #sudo rm -rf /data/traces/*
-#yes | sudo GOMP_CPU_AFFINITY="0-2" OMP_SCHEDULE=static OMP_NUM_THREADS=3 ./benchmark.sh mmult_eigen_par 101676 ./cpp/mmult_eigen_par 4 4096 mat
-#sudo mv experiment_results/mmult_eigen_par experiment_results/mmult_eigen_par_3
+sudo GOMP_CPU_AFFINITY="0-2" OMP_SCHEDULE=static OMP_NUM_THREADS=3 ./trace_benchmark.sh mmult_eigen_par 101676 ./cpp/mmult_eigen_par 4 4096 mat
+sudo mv tracing_results/mmult_eigen_par tracing_results/mmult_eigen_par_3
 
 #sudo rm -rf /data/traces/*
-#yes | sudo GOMP_CPU_AFFINITY="0-3" OMP_SCHEDULE=static OMP_NUM_THREADS=4 ./benchmark.sh mmult_eigen_par 101543 ./cpp/mmult_eigen_par 4 4096 mat
+sudo GOMP_CPU_AFFINITY="0-3" OMP_SCHEDULE=static OMP_NUM_THREADS=4 ./trace_benchmark.sh mmult_eigen_par 101543 ./cpp/mmult_eigen_par 4 4096 mat
 #sudo mv experiment_results/mmult_eigen_par experiment_results/mmult_eigen_par_4
 
 #sudo rm -rf /data/traces/*
-#yes | sudo ./benchmark.sh mmult_eigen_vec 524718 taskset -c 0 ./cpp/mmult_eigen_vec 4 16384 vec
+sudo ./trace_benchmark.sh mmult_eigen_vec 524718 taskset -c 0 ./cpp/mmult_eigen_vec 4 16384 vec
 
 #sudo rm -rf /data/traces/*
-#yes | sudo ./benchmark.sh mmult_eigen_dot 524652 taskset -c 0 ./cpp/mmult_eigen_dot 4 134217728 dot
+sudo ./trace_benchmark.sh mmult_eigen_dot 524652 taskset -c 0 ./cpp/mmult_eigen_dot 4 134217728 dot
 
 #sudo rm -rf /data/traces/*
-#yes | sudo ./benchmark.sh sort_merge 262921 taskset -c 0 ./cpp/sort_merge 42 268435456 bitonic_merge false
+sudo ./trace_benchmark.sh sort_merge 262921 taskset -c 0 ./cpp/sort_merge 42 268435456 bitonic_merge false
 
 #sudo rm -rf /data/traces/*
-#yes | sudo ./benchmark.sh sparse_eigen 296292 taskset -c 0 ./cpp/sparse_eigen 4 5500 false
+sudo ./trace_benchmark.sh sparse_eigen 296292 taskset -c 0 ./cpp/sparse_eigen 4 5500 false
